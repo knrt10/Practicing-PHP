c72907366c87a4c858e7a3a51d16b7194e710968
Kautilya Tripathi <Kautilya Tripathi>
2017-07-16 04:38:53 +0530

    initial commit

---
+++ 23rd(MD5 encryption)/md5.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+        
+        MD5 hashing is a one sided encryption . You cannot change back hashed part to normal string.
+        Also it is not so much secured was of hashing as it can be exploited by brute force attack.
+        But it can be used when storing data to databases.
+        
+*/
+
+if(isset($_POST['password'])){
+    if(!empty($_POST['password'])){
+        $password = md5($_POST['password']);
+        
+        $handle = fopen('hash.txt','r');
+        $passhash = fread($handle,filesize('hash.txt'));//password is password
+        if($password==$passhash){
+            echo "Password OK";
+        }else{
+            echo "Not correct";
+        }
+    }
+}
+?>
+
+<form action="md5.php" method="post">
+Password : <br/><br/>
+    <input type="text" name="password" />
+    <br/><br/>
+    <input type="submit" value="Submit"/>
+</form>
